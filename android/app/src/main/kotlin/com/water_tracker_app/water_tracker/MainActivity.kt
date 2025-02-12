fd12f431c5f7d96daf208bdc77e7a12edbf15161
masumrashid <razzshikder@gmail.com>
2025-02-13 02:05:55 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/water_tracker_app/water_tracker/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.water_tracker_app.water_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Water Tracter'),
+      ),
+    );
+  }
+}
+
